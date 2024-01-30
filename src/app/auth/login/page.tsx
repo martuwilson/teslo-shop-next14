f58e1e9ac78284a363870b13782de79a97eb8b49
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-01-30 20:49:48 -0300

    folder structure && first pages

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>
+        Log page
+      </h1>
+    </main>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,12 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>
+        New Account
+      </h1>
+    </main>
+  );
+}
