69bf053abbb86caf573906ec8af60cf8a6dc3613
Martin Ezequiel Williner <111790529+martuwilson@users.noreply.github.com>
2024-02-03 20:04:00 -0300

    MouseOver => change image like a hover

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,21 +1,33 @@
+'use client';  // al usar hooks automaticamente debe ser del lado del cliente
+
 import { Product } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface ProductItemProps {
   product: Product; // es sin [] porque product
 }
 
 export const ProductGridItem = ({ product }: ProductItemProps) => {
+
+    const [displayImage, setDisplayImage] = useState(product.images[0])
+
   return (
     <div className="rounded-md overflow-hidden fade-in">
       <Link href={`/product/${product.slug}`}>
         <Image
-          src={`/products/${product.images[0]}`}
+          src={`/products/${displayImage}`}
           alt={product.title}
           className="w-full object-cover"
           width={500}
           height={500}
+          onMouseEnter={() => {
+            setDisplayImage(product.images[1]);
+          }}
+            onMouseLeave={() => {
+                setDisplayImage(product.images[0]);
+            }}
         />
       </Link>
       <div className="p-4 flex flex-col">
