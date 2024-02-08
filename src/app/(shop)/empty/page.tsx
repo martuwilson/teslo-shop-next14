f2250531914f8cd1d388356c7eab1a9dc5889d4b
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-02-08 16:40:35 -0300

    Empty cart page added

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/display-name */
+
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+
 /* eslint-disable import/no-anonymous-default-export */
 export default function(){
     return (
-        <div className="min-h-screen bg-red-400">
-          <h1>
-            empty page
-          </h1>
+        <div className="flex justify-center items-center h-[800px]">
+          <IoCartOutline size={80} className="mx-5"/>
+
+          <div
+            className="flex flex-col items-center"
+          >
+            <h1 className="text-xl font-semibold">
+            Tu carrito esta vacio
+            </h1>
+
+            <Link href="/" className="text-blue-500 mt-2 text-4xl">
+              Regresar
+            </Link>
+          </div>
         </div>
       );
 }
