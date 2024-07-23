78eea6c083c70377d76be86f054af9728eeba6ab
Martin Ezequiel Williner <111790529+martuwilson@users.noreply.github.com>
2024-07-23 01:37:54 -0300

    next fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { titleFont } from "@/config/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
+        Hola mundo
+      </h1>
+      <h1 className={
+        titleFont.className
+      }>
         Hola mundo
       </h1>
     </main>
