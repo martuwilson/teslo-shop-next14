e7f3b2892294943472b7adee9674b4c73e2dafda
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-07-25 13:03:56 -0300

    TopMenu agregado

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,10 +1,13 @@
+import { TopMenu } from "@/components"
+
 export default function ShopLayout ({
     children
 }: {
     children?: React.ReactNode
 }) {
     return (
-        <main className="min-h-screen bg-red-500">
+        <main className="min-h-screen">
+            <TopMenu />
             {children}
         </main>
     )
