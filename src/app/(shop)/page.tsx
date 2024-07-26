e2284b16c29e42ab8cd2b014badd85e23cfeb894
Martin Ezequiel Williner <111790529+martuwilson@users.noreply.github.com>
2024-07-26 11:48:10 -0300

    Title component added

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,17 +1,11 @@
+import { TitleComponent } from "@/components";
 import { titleFont } from "@/config/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Hola mundo
-      </h1>
-      <h1 className={
-        titleFont.className
-      }>
-        Hola mundo
-      </h1>
-    </main>
+    <>
+      <TitleComponent title="Tienda" subtitle="Todos los productos" className="mb-2"/>
+    </>
   );
 }
