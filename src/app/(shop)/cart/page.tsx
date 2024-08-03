51fcfeeb00ef79497ea42135478d5639e6131b45
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-08-03 19:12:21 -0300

    Carrito de compras agregado

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,9 +1,106 @@
+import { QuantitySelector, TitleComponent } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+
+const productsInCart = [
+    initialData.products[0],
+    initialData.products[1],
+    initialData.products[2],
+]
+
 export default function() {
     return (
-       <div>
-        <h1>
-            cart page
-        </h1>
+       <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+        <div className="flex flex-col w-[1000px]">
+            <TitleComponent title="Carrito" />
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+                
+                {/* carrito */}
+                <div className="flex flex-col mt-5">
+                    <span className="text-xl">
+                        Agregar mas items
+                    </span>
+                    <Link href="/" className="underline mb-5">
+                    Continuar comprando
+                    </Link>
+                
+
+                {/* items */}
+                {
+                    productsInCart.map(prod => (
+                        <div key={prod.slug} className="flex mb-5">
+                            <Image
+                                src={`/products/${prod.images[0]}`}
+                                width={100}
+                                height={100}
+                                alt={prod.title}
+                                style={{
+                                    width: '100px',
+                                    height: '100px',
+                                }}
+                                className="mr-5 rounded"
+                            />
+
+                            <div>
+                                <p>{prod.title}</p>
+                                <p>{prod.price}</p>
+                                <QuantitySelector quantity={4}/>
+
+                                <button className="underline mt-3">
+                                    Remover
+                                </button>
+                            </div>
+
+                        </div>
+                    ))
+                }
+                </div>
+                {/* resumen de orden */}
+                <div className="bg-white rounded-xl shadow-xl p-7">
+                    <h2 className="text-2xl mb-2">
+                        Resumen de orden
+                    </h2>
+                    <div className="grid grid-cols-2">
+                        <span>
+                            No. Productos
+                        </span>
+                        <span className="text-right">
+                            3 artículos
+                        </span>
+
+                        <span>
+                            Subtotal
+                        </span>
+                        <span className="text-right">
+                            $100
+                        </span>
+
+                        <span>
+                            Impuestos (15%)
+                        </span>
+                        <span className="text-right">
+                            $13
+                        </span>
+
+                        <span className="mt-5 text-2xl">
+                            Total:
+                        </span>
+                        <span className="mt-5 text-2xl text-right">
+                            $113
+                        </span>
+                    </div>
+
+                    <div className="mt-5 mb-2 w-full">
+                        <Link href="/checkout/address" className="flex btn-primary justify-center">
+                        Finalizar orden
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
        </div>
     )
 }
